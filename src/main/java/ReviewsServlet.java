5f2a876d7e4eb0077ea1164f25601e4bcd8f541f
Daniil Nikulin <danil.nikulin@gmal.com>
2017-11-06 17:03:24 +0200

    Reviews 4 доделано

--- src/main/java/ReviewsServlet.java
+++ src/main/java/ReviewsServlet.java
@@ -16,7 +16,7 @@ public class ReviewsServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-
+        //Reviews.reviews.add(new Reviews.Review("Vasiya","Thir Repo is awesome",2));
         request.setAttribute("reviews", Reviews.reviews);
         response.setContentType("text/html");
         RequestDispatcher dispatcher = request.getRequestDispatcher("/Reviews.jsp");
@@ -25,11 +25,11 @@ public class ReviewsServlet extends HttpServlet {
     }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        String content = request.getParameter("review_text");
+        String textField = request.getParameter("review_text");
         String name = request.getParameter("name");
         int givenRating = Integer.parseInt(request.getParameter("star"));
-        if((!Objects.equals(content, "")) && (!Objects.equals(name, "")) && (givenRating <5 && givenRating>1)){
-            Reviews.reviews.add(new Reviews.Review("Vasiya","Thir Repo is awesome",givenRating));
+        if((!Objects.equals(textField, "")) && (!Objects.equals(name, "")) && (givenRating <5 && givenRating>1)){
+            Reviews.reviews.add(new Reviews.Review(name,textField,givenRating));
             doGet(request, response);
         }
 
